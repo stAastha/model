2b172257d779c5a6b91bd2e01d9ff8307778c51b
Aastha <aasthathakur1206@gmail.com>
2024-05-13 17:32:17 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    Name= models.CharField(max_length=50)
+    Email= models.EmailField()
+    Contact= models.IntegerField()
+    City= models.CharField(max_length=50)
+    
+
+
